f3b45f9fe2d7b607e9d333272f293cafaac6981c
Tecknik <manu.s.bhat@gmail.com>
2019-09-18 17:28:41 -0700

    Made revisions to math ops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumMathOps.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumMathOps.java
@@ -3,28 +3,39 @@ package org.firstinspires.ftc.teamcode;
 public class MecanumMathOps {
     //
     private double speed = 0;
+    //
+    private double flPower = 0;
+    private double frPower = 0;
+    private double blPower = 0;
+    private double brPower = 0;
 
 
-    public static double getFrontLeftMotorP(double x,double y,double r){
-        return x + y + r;
+    public double getFrontLeftMotorP(double x,double y,double r){
+        return this.flPower;
     }//this is an edit
 
-    public static double getFrontRightMotorP(double x,double y,double r){
-        return x - y - r;
+    public double getFrontRightMotorP(double x,double y,double r){
+        return this.frPower;
     }
 
-    public static double getBackLeftMotorP(double x,double y,double r){
-        return x - y + r;
+    public double getBackLeftMotorP(double x,double y,double r){
+        return this.blPower;
     }
 
-    public static double getBackRightMotorP(double x,double y,double r){
-        return x - y + r;
+    public double getBackRightMotorP(double x,double y,double r){
+        return this.brPower;
     }
 
     public void accelerate(double deltaSpeed,long milliseconds) {
-
+        //to be implemented
     }
 
+    public void strafeAndTurn(double x,double y,double r){
+        this.flPower = x + y + r;
+        this.frPower = x - y - r;
+        this.blPower = x - y + r;
+        this.brPower = x + y + r;
+    }
     public void update(long dt) {
 
     }
